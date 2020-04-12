d9cc7da456510ce69c6b7642197c8c5909120e1a
Anthony Thomas <thomas9t@users.noreply.github.com>
2020-04-11 17:33:51 -0700

    Create params.py

---
+++ src/params.py
@@ -0,0 +1,12 @@
+DEBUG = True
+SEG_N = 512
+if DEBUG:
+    latent_dim_list = [16]
+    beta_list       = [1e-3]
+    lr_list         = [1e-8]
+    decay_list      = [1e-5]
+else:
+    latent_dim_list = [1, 2, 4, 16]
+    beta_list       = [1e-7, 1e-5, 1e-3, 1e-2]
+    lr_list         = [1e-7, 1e-5]
+    decay_list      = [1e-5, 1e-3]
