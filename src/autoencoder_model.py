2892664c797e165421776be02ab8df3faa60f56f
Alireza <alireza.amirshahi@epfl.ch>
2020-06-24 02:26:37 +0200

    prepare data and train the autoencoder model

--- src/autoencoder_model.py
+++ src/autoencoder_model.py
@@ -9,24 +9,24 @@ from tensorflow.keras.utils import plot_model
 def build_model(input_shape=None,
                 label_shape = None,
                 enc_dimension=None,
-                optim=None,
-                FS = None):
-    z1_input = layers.Input(shape=(input_shape))
+                optim=None):
+    z1_input = layers.Input(shape=input_shape)
     label_input = layers.Input(shape=label_shape)
 
     concat_input = layers.Concatenate()([z1_input, label_input])
-    dense1 = layers.Dense(12, activation='relu')(concat_input)
+    # dense1 = layers.Dense(32, activation='relu')(concat_input)
+    dense1 = layers.Dense(32, activation='relu')(z1_input)
     latent = layers.Dense(enc_dimension, activation='relu')(dense1)
-    dense3 = layers.Dense(12, activation='relu')(latent)
+    dense3 = layers.Dense(32, activation='relu')(latent)
     recons = layers.Dense(input_shape[0], activation=None)(dense3)
 
     # recons_cost = K.mean(losses.mse(concat_input, recons))
 
-    model = models.Model(inputs=[z1_input, label_input], outputs=recons, name="encoder")
+    model = models.Model(inputs=z1_input, outputs=recons, name="encoder")
     model.compile(optimizer=optim, loss = "mse")
     return model
 
 
 if __name__ == '__main__':
-    model = build_model(input_shape=(16,), enc_dimension=8, optim='adam', FS= 256, label_shape=(1,))
+    model = build_model(input_shape=(16,), enc_dimension=8, optim='adam', label_shape=(1,))
     plot_model(model, '../output/AE_model.png', show_shapes=True, show_layer_names=False)
--- src/vae_mmd.py
+++ src/vae_mmd.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 from scipy import signal, integrate
 import vae_model
+import autoencoder_model
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 from sklearn.metrics.pairwise import rbf_kernel, polynomial_kernel, linear_kernel
@@ -12,6 +13,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from utils import create_seizure_dataset
+import pickle
 
 sys.path.append("../")
 LOG = logging.getLogger(os.path.basename(__file__))
@@ -38,17 +40,45 @@ def get_TSNE(x):
     return x_embedded
 
 
+def get_mmd(kernel, latent):
+    K = kernel(latent)
+    mmd = []
+    input_length = latent.shape[0]
+    for N in range(1, input_length):
+        M = input_length - N
+        Kxx = K[:N, :N].sum()
+        Kxy = K[:N, N:].sum()
+        Kyy = K[N:, N:].sum()
+        mmd.append(np.sqrt(
+            ((1 / float(N * N)) * Kxx) +
+            ((1 / float(M * M)) * Kyy) -
+            ((2 / float(N * M)) * Kxy)
+        ))
+
+    ws = []
+    mmd = np.array(mmd)
+    mmd_corr = np.zeros(mmd.size)
+    N = input_length - 1
+    for ix in range(1, mmd_corr.size):
+        w = (N / float(ix * (N - ix)))
+        ws.append(w)
+        mmd_corr[ix] = mmd[ix] - w * mmd.max()
+
+    arg_max_mmd = np.argmax(mmd_corr[EXCLUDED_SIZE:-EXCLUDED_SIZE]) + EXCLUDED_SIZE
+    return arg_max_mmd
+
+
 def main():
     dirname = "../temp/vae_mmd"
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
-    arch = 'psd_non_single_seizure'
+    arch = 'psd'
     beta = 0.001
-    latent_dim = 16
+    latent_dim = 64
     lr = 0.0001
     decay = 0.0
-    gamma = 5000000.0
+    gamma = 500000.0
 
     root = "../output/vae/{}/".format(arch)
     stub = "seg_n_{}/beta_{}/latent_dim_{}/lr_{}/decay_{}/gamma_{}"
@@ -66,7 +96,7 @@ def main():
     model, _ = build_model(**build_model_args)
 
     if not os.path.exists(dirname):
-        print("Model does not exist")
+        print("Model does not exist in {}".format(dirname))
         exit()
     model.load_weights(dirname)
     intermediate_model = tf.keras.models.Model(inputs=model.inputs, outputs=model.layers[1].output)
@@ -84,45 +114,43 @@ def main():
 
     sessions = create_seizure_dataset(SEG_LENGTH, SF)
     LOG.info("session number: {}".format(len(sessions.keys())))
-
-    for node in sessions.keys():    # Loop1: cross validation
+    ae_model_args = {
+        "input_shape": (latent_dim,),
+        "label_shape": (1,),
+        "enc_dimension": 16,
+        "optim": Adam(learning_rate=0.00001),
+    }
+    for node in sessions.keys():  # Loop1: cross validation
         test_patient = node
         train_patients = [p for p in sessions.keys() if p != node]
-        for epoch in range(1):      # Loop2: epochs
+        ae_model = autoencoder_model.build_model(**ae_model_args)
+        for epoch in range(1):  # Loop2: epochs
+            Z1_data = np.zeros((0, latent_dim))
+            Z1_label = np.zeros((0,))
             for patient in train_patients:  # Loop3: train patients
+                LOG.info("patient: {}".format(patient))
                 X = sessions[patient]['data']
                 latent = intermediate_model.predict(X)[2]
+                Z1_data = np.concatenate((Z1_data, latent))
+
+                mmd_maximum = get_mmd(kernel, latent)
+                y_label = np.zeros(latent.shape[0])
+                y_label[mmd_maximum - EXCLUDED_SIZE:mmd_maximum + EXCLUDED_SIZE] = 1
+                Z1_label = np.concatenate((Z1_label, y_label))
+
+            ae_model.fit(x=Z1_data, y= Z1_data, epochs=10, batch_size=64)
+        break
+
+        # X_test = sessions[test_patient]['data']
+        # latent = intermediate_model.predict(X_test)[2]
+
+
     # y_non_zero = np.where(y > 0, 1, 0)
     # y_diff = np.diff(y_non_zero)
     # start_points = np.where(y_diff > 0)[0]
     # stop_points = np.where(y_diff < 0)[0]
     # middle_points = (start_points + stop_points) // 2
     # LOG.info("points: {}, {}".format(start_points, stop_points))
-
-                K = kernel(latent)
-                mmd = []
-                for N in range(1, X.shape[0]):
-                    M = X.shape[0] - N
-                    Kxx = K[:N, :N].sum()
-                    Kxy = K[:N, N:].sum()
-                    Kyy = K[N:, N:].sum()
-                    mmd.append(np.sqrt(
-                        ((1 / float(N * N)) * Kxx) +
-                        ((1 / float(M * M)) * Kyy) -
-                        ((2 / float(N * M)) * Kxy)
-                    ))
-
-                ws = []
-                mmd = np.array(mmd)
-                mmd_corr = np.zeros(mmd.size)
-                N = X.shape[0] - 1
-                for ix in range(1, mmd_corr.size):
-                    w = (N / float(ix * (N - ix)))
-                    ws.append(w)
-                    mmd_corr[ix] = mmd[ix] - w * mmd.max()
-
-                arg_max_mmd = np.argmax(mmd_corr[EXCLUDED_SIZE:-EXCLUDED_SIZE]) + EXCLUDED_SIZE
-                LOG.info("patient: {}, max mmd: {}". format(patient, arg_max_mmd))
 
     # plt.figure()
     # plt.plot(mmd_corr, label="MMD")
@@ -143,6 +171,6 @@ def main():
     # plt.close()
 
 
-if __name__=="__main__":
-    # tf.config.experimental.set_visible_devices([], 'GPU')
+if __name__ == "__main__":
+    tf.config.experimental.set_visible_devices([], 'GPU')
     main()
