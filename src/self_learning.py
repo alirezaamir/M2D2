594a3b0c64c76a7afc66838ec31ba8a3a2d28e35
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2021-04-08 17:24:20 +0200

    borrow seizure signals from Epilepsiae for the self learning

--- src/self_learning.py
+++ src/self_learning.py
@@ -11,20 +11,21 @@ TH = 0.5
 
 
 def borrow_seizure():
-    pat_rand = np.random.randint(1,24)
-    dataset = test_dataset(pat_rand)
     x_borrow = np.zeros((0, 899, 1024, 2))
     y_borrow = np.zeros((0, 899))
 
-    for session in dataset.keys():
-        x_in = dataset[session]['data']
-        y_in = dataset[session]['label']
-        if np.sum(y_in) == 0:
-            continue
-        if x_in.shape[0] != 899:
-            continue
-        x_borrow = np.concatenate((x_borrow, np.expand_dims(x_in, 0)))
-        y_borrow = np.concatenate((y_borrow, np.expand_dims(y_in, 0)))
+    pat_rand = np.random.randint(1, 24, size=3)
+    for pat in pat_rand:
+        dataset = test_dataset(pat)
+        for session in dataset.keys():
+            x_in = dataset[session]['data']
+            y_in = dataset[session]['label']
+            if np.sum(y_in) == 0:
+                continue
+            if x_in.shape[0] != 899:
+                continue
+            x_borrow = np.concatenate((x_borrow, np.expand_dims(x_in, 0)))
+            y_borrow = np.concatenate((y_borrow, np.expand_dims(y_in, 0)))
     return x_borrow, y_borrow
 
 
@@ -35,31 +36,39 @@ def self_eval(model, test_patient):
     filenames = ["{}/{}_{}.pickle".format(dirname, test_patient, i) for i in range(pat_hours[test_patient]//2,
                                                                                    pat_hours[test_patient])]
 
+    error = []
     for filename in filenames:
         with open(filename, "rb") as pickle_file:
             data = pickle.load(pickle_file)
             x = np.array(data["X"])
             y = np.array(data["y"])
-            # if x.shape[0] != 899:
-            #     print("Shape: {} removed".format(x.shape[0]))
-            #     continue
-            # x = np.pad(x, [(0,1),(0,0), (0,0)], mode= 'edge')
-            # x = np.reshape(x, (-1, 300, 1024, 2))
-            #
-            # y = np.pad(y, (0,1), mode='edge')
-            # y = np.reshape(y, (-1, 300))
+            if x.shape[0] != 899:
+                print("Shape: {} removed".format(x.shape[0]))
+                continue
+            x = np.pad(x, [(0,1),(0,0), (0,0)], mode= 'edge')
+            x = np.reshape(x, (-1, 300, 1024, 2))
 
-            x = np.expand_dims(x, axis=0)
+            y = np.pad(y, (0,1), mode='edge')
+            y = np.reshape(y, (-1, 300))
 
-            mmd_predict = model.predict(x, batch_size = 1)
-            max_mmd = np.max(mmd_predict)
-            label = 1 if np.sum(y) > 0 else 0
-            x_roc.append(max_mmd)
-            y_roc.append(label)
+            for idx in range(x.shape[0]):
+                x_in = np.expand_dims(x[idx], axis=0)
+                mmd_predict = model.predict(x_in, batch_size = 1)
+                max_mmd = np.max(mmd_predict)
+                label = 1 if np.sum(y[idx]) > 0 else 0
+                x_roc.append(max_mmd)
+                y_roc.append(label)
+
+                if label == 1:
+                    middle = np.median(np.where(y[idx]>0)[0])
+                    err = np.abs(np.argmax(mmd_predict) - middle)
+                    error.append(err)
+                    print("Error : {}, predict :{}".format(err, np.argmax(mmd_predict)))
 
     auc = roc_auc_score(y_true=y_roc, y_score=x_roc)
     print("AUC: {}".format(auc))
-    return auc
+
+    return auc, error
 
 
 def get_data(test_patient):
@@ -84,19 +93,21 @@ def get_data(test_patient):
 
 
 def self_train(test_patient):
-    source_arch = 'vae_sup_chb_total'
-    source_model = 'vae_total_v26'
+    source_arch = 'vae_sup_chb'
+    source_model = 'vae_interval_20min_v24'
     save_path = '../temp/vae_mmd/integrated/{}/{}/{}/model/test_{}/saved_model/'.format(SEG_N,
                                                                                         source_arch,
                                                                                         source_model,
                                                                                         '-1')
     trained_model = tf.keras.models.load_model(save_path)
-    for layer in trained_model.layers[:18]:
-        print(layer.name)
-        layer.trainable = False
+    # for layer in trained_model.layers[:18]:
+    #     print(layer.name)
+    #     layer.trainable = False
     auc_array = []
-    auc = self_eval(trained_model, test_patient)
+    error_array = []
+    auc, error = self_eval(trained_model, test_patient)
     auc_array.append(auc)
+    error_array.append(error)
 
     x, y = get_data(test_patient)
     x_seizure = np.zeros((0, 899, 1024, 2))
@@ -129,8 +140,9 @@ def self_train(test_patient):
             trained_model.fit(x=x_train, y=label_train, batch_size=1, epochs=4)
         h += 1
         if h%5 == 0:
-            auc = self_eval(trained_model, test_patient)
+            auc, error = self_eval(trained_model, test_patient)
             auc_array.append(auc)
+            error_array.append(error)
     print("AUC :{}".format(auc_array))
 
 
