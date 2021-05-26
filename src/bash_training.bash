8b0141483848aab1cbd30e2a8ec6c99f2f9b35f4
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2021-05-26 11:13:02 +0200

    add nested loop in the bash

--- src/bash_training.bash
+++ src/bash_training.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
-for i in {1..23}
+for l in 2 4 8 16 32
+do
+	for i in {1..23}
 do
 	echo $i
-	python training.py vae_sup_chb 0.00001 4 0.0001 0.02 0 $i
+	python training.py vae_sup_chb 0.00001 $l 0.0001 0.02 0 $i
+	done
 done
